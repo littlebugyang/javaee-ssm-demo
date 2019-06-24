e8339f995a83e5a95d9fc6fccd640eea4932a9dc
littlebugyang <im.lby@qq.com>
2019-06-24 13:51:36 +0800

    给Controller添加注解, 匹配了路由, 访问localhost:8081/getUser可以得到字符串; DemoApplication无法检测到其他包内的controller, 所以添加ComponentScan注解; 统一了一下包名

--- src/main/java/com/nocodingnomoney/demo/DemoApplication.java
+++ src/main/java/com/nocodingnomoney/demo/DemoApplication.java
@@ -1,13 +1,15 @@
 package com.nocodingnomoney.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.nocodingnomoney.mapper")
+@ComponentScan({"com.nocodingnomoney.controller"})
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/nocodingnomoney/entity/User.java
@@ -0,0 +1,4 @@
+package com.nocodingnomoney.entity;
+
+public class User {
+}
