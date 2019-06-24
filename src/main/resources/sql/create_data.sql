c1308242c0abc7557aedf033c9f6d0aac62f907b
littlebugyang <im.lby@qq.com>
2019-06-24 10:59:57 +0800

    加入了插件: Alibaba Java Code Guidelines; 添加了创建测试数据的SQL语句; 添加了Controller和Entity装模作样

---
+++ src/main/resources/sql/create_data.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE mybatis_test;
+
+CREATE TABLE user
+(
+    age INTEGER NOT NULL,
+    name VARCHAR(64) NOT NULL DEFAULT ''
+
+);
+
+insert user values(18,'zhanjindong');
+insert user values(20,'zhangsan');
